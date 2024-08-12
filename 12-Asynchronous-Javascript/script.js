cb6f1a9fa6f3b2dd7fa90d7b3aaf306c0b5c52d7
samarjeet-singh-kheda <samarjeetsingh259@gmail.com>
2024-08-12 23:11:00 +0530

    FIXED: minor clashes in script loading due to same variable names

--- 12-Asynchronous-Javascript/script.js
+++ 12-Asynchronous-Javascript/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// const btn = document.querySelector(".btn-country");
-// const countriesContainer = document.querySelector(".countries");
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 const renderCountry = function (data, className = "") {
   const html = `
